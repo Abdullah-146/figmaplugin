a46653c44b47e83bb4c3937ab79451558f765c43
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-12-02 01:29:16 +0500

    comitted

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 })
 
-app.post('/svg-converter', (req, res) => {
-    
-  try{
+app.post('/svg-converter', async (req, res) => {
+
+  try {
     let svgTags = req.body.svgTags;
+    console.log('====================================');
+    console.log(svgTags);
+    console.log('====================================');
 
     //convert svg tags to base64
-    let base64 = Buffer.from(svgTags).toString('base64');
+    let base64 = await Buffer.from(svgTags).toString('base64');
+    console.log('====================================');
+    console.log(base64);
+    console.log('====================================');
 
     //return base64
-    res.send(base64);
+    res.status(200).send({ data: base64 });
 
-  }catch(err){
+  } catch (err) {
     res.status(500).send(err);
   }
 
@@ -26,5 +38,5 @@ app.post('/svg-converter', (req, res) => {
 
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000")
+  console.log("Server running on port 5000")
 });
