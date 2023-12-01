fafa452b091a7c30588ae97426fc91b6cdb82549
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-12-02 00:29:35 +0500

    Suleman: Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.post('/svg-converter', (req, res) => {
+    
+  try{
+    let svgTags = req.body.svgTags;
+
+    console.log("SVG TAGS: ",svgTags);
+    
+    //convert svg tags to base64
+    let base64 = Buffer.from(svgTags).toString('base64');
+
+    //return base64
+    res.send(base64);
+
+  }catch(err){
+    res.status(500).send(err);
+  }
+
+})
+
+
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
+});
